436fbf00d3b50c71c819ce416bb278c4642accf2
Icerock10 <dp021190dms@gmail.com>
2022-01-20 22:00:35 +0200

    Added keyboard

---
+++ wordle-app/src/App/App.js
@@ -0,0 +1,12 @@
+import Keyboard from "./Keyboard";
+
+const App = () => {
+  return (
+    <div className="flex-initial items-center flex-col m-auto max-w-2xl">
+          <div>Wordle game</div>
+        <Keyboard />
+    </div>
+  );
+}
+
+export default App;
